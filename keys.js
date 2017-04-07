b1daeb659b3afd6ecf895586b7b54583b7c2edc8
Rima Alrwais <ralrwais@ralrwais-ltm2.internal.salesforce.com>
2017-04-07 09:38:49 -0700

    Setup

---
+++ keys.js
@@ -0,0 +1,8 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: 'xVzrSwWnGiJw1HH632meTgqze',
+  consumer_secret: '7JbiaJfrRoqY1PxzsvAbj1EvscAghJUp01iKdZVPO2GzJtIiIL',
+  access_token_key: '850051946807414784-2JdtfX1DUXMGO5G5Txiq5aY5p5Lv1G4',
+  access_token_secret: 'EJurcALlC39hJz27mYs8qrpDRQoVxt5mAPIzO3h2MLPYB',
+}
---
+++ liri.js
@@ -0,0 +1,27 @@
+//write the code you need to grab the data from keys.js. 
+//Then store the keys in a variable
+
+//command: my-tweets(show your last 20 tweets and when they were created at in your terminal/bash window.)
+
+//command: spotify-this-song(in terminal followed by : '<song name here>') 
+//^^ will show:Artist(s) /The song's name / A preview link of the song from Spotify/ The album that the song is from
+//if no song is provided then your program will default to "The Sign" by Ace of Base
+
+
+//command: movie-this (in terminal followed by '<movie name here>')
+//Title of the movie.
+   * Year the movie came out.
+   * IMDB Rating of the movie.
+   * Country where the movie was produced.
+   * Language of the movie.
+   * Plot of the movie.
+   * Actors in the movie.
+   * Rotten Tomatoes Rating.
+   * Rotten Tomatoes URL.
+//If the user doesn't type a movie in, the program will output data for the movie 'Mr. Nobody.'
+
+
+
+//command: do-what-it-says
+//Using the fs Node package, LIRI will take the text inside of random.txt and then use it to call one of LIRI's commands
+
